b74a07b32a774854c5f0ef0d3a6fc115db8df661
godizord <luisleongx@gmail.com>
2020-04-25 21:12:52 -0400

    endpoint recibopoliza, se comenta campo ReciboPoliza de endpoint poliza y polizaauto

---
+++ brokfy.dashboard.api/Controllers/ReciboPolizaController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using brokfy.dashboard.api.data.DataModel;
+using Microsoft.Extensions.Configuration;
+using brokfy.dashboard.api.Models;
+using System;
+
+namespace brokfy.dashboard.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReciboPolizaController : ControllerBase
+    {
+        private readonly brokfy_devContext _context;
+        private IConfiguration _config;
+        public ReciboPolizaController(brokfy_devContext context, IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+        }
+
+        // GET: api/ReciboPoliza/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<dynamic>> GetReciboPoliza(string id)
+        {
+            string dato = _context.Polizas.Where(x => x.NoPoliza == id).FirstOrDefault().ReciboPdf;
+
+            if (dato == null)
+            {
+                return new { ReciboPdf = "" };
+            }
+
+            return new { ReciboPdf = dato };
+        }
+    }
+}

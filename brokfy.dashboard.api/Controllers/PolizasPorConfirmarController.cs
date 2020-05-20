c7b5c76615cf548b8a0db8736302f12c8b5158eb
godizord <luisleongx@gmail.com>
2020-05-19 20:43:46 -0400

    nuevo endpoint polizasporconfirmar

---
+++ brokfy.dashboard.api/Controllers/PolizasPorConfirmarController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using brokfy.dashboard.api.data.DataModel;
+using Microsoft.Extensions.Configuration;
+using brokfy.dashboard.api.Models;
+using System;
+
+namespace brokfy.dashboard.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PolizasPorConfirmarController : ControllerBase
+    {
+        private readonly brokfy_devContext _context;
+        private IConfiguration _config;
+        public PolizasPorConfirmarController(brokfy_devContext context, IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+        }
+
+        // GET: api/PolizasPorConfirmar
+        [HttpGet]
+        public IEnumerable<Polizas> GetPolizasPorConfirmar()
+        {
+            return _context.Polizas.Where(x => x.IdEstadoPoliza == 4).ToList();
+        }
+    }
+}

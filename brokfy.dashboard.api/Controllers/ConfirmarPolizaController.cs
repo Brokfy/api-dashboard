9ac8394175ce25c66ce5f5da69b0bbdfeab07ffd
godizord <luisleongx@gmail.com>
2020-05-21 00:42:47 -0400

    nuevo endpoint ConfirmarPoliza y ValorCOmision

---
+++ brokfy.dashboard.api/Controllers/ConfirmarPolizaController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using brokfy.dashboard.api.data.DataModel;
+using Microsoft.Extensions.Configuration;
+using brokfy.dashboard.api.Models;
+using System;
+
+namespace brokfy.dashboard.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfirmarPolizaController : ControllerBase
+    {
+        private readonly brokfy_devContext _context;
+        private IConfiguration _config;
+        public ConfirmarPolizaController(brokfy_devContext context, IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+        }
+
+        // PUT: api/ConfirmarPoliza
+        [HttpPut]
+        public ResponseModel PutConfirmarPoliza([FromBody] string NoPoliza)
+        {
+            try
+            {
+                Polizas poliza = _context.Polizas.Where(x => x.NoPoliza == NoPoliza).FirstOrDefault();
+                poliza.IdEstadoPoliza = 1;
+                _context.Polizas.Update(poliza);
+                _context.SaveChanges();
+                return new ResponseModel { Message = "Ok", Result = null, Success = true };
+            }
+            catch (Exception ex)
+            {
+
+                return new ResponseModel { Message = ex.InnerException != null ? ex.InnerException.Message : ex.Message, Result = null, Success = false };
+            }
+        }
+    }
+}
---
+++ brokfy.dashboard.api/Controllers/ValorComisionController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using brokfy.dashboard.api.data.DataModel;
+using Microsoft.Extensions.Configuration;
+using brokfy.dashboard.api.Models;
+using System;
+
+namespace brokfy.dashboard.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValorComisionController : ControllerBase
+    {
+        private readonly brokfy_devContext _context;
+        private IConfiguration _config;
+        public ValorComisionController(brokfy_devContext context, IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+        }
+
+        // GET: api/ValorComision
+        [HttpGet]
+        public double GetValorComision([FromQuery] int IdAseguradora, [FromQuery] int IdTipoPoliza, [FromQuery] DateTime Fecha)
+        {
+            return _context.AseguradorasComisiones.Where(x =>
+            x.IdAseguradora == IdAseguradora
+            && x.IdTipoPoliza == IdTipoPoliza
+            && Fecha > x.FechaInicioVigencia
+            && x.FechaFinVigencia == null).FirstOrDefault().Valor;
+        }
+    }
+}

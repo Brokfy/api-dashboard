bb6c02e673562f75d6cbd9387532d26b073e0d5b
Hamid <hapsa10@gmail.com>
2020-06-21 14:55:40 -0400

    Cambio de campos opcionales en la respuesta de los nuevos reportes

--- brokfy.dashboard.api.data/ViewModel/ReporteHistoricoPorcentajeComisiones.cs
+++ brokfy.dashboard.api.data/ViewModel/ReporteHistoricoPorcentajeComisiones.cs
@@ -8,16 +8,16 @@ namespace brokfy.dashboard.api.data.ViewModel
     {
         public int TipoRegistro { get; set; }
         public string Aseguradora { get; set; }
-        public DateTime FechaCambio { get; set; }
-        public double Auto { get; set; }
-        public double Moto { get; set; }
-        public double Hogar { get; set; }
-        public double Salud { get; set; }
-        public double Vida { get; set; }
-        public double Gadget { get; set; }
-        public double Mascota { get; set; }
-        public double Viaje { get; set; }
-        public double Retiro { get; set; }
-        public double Pyme { get; set; }
+        public DateTime ?FechaCambio { get; set; }
+        public double ?Auto { get; set; }
+        public double ?Moto { get; set; }
+        public double ?Hogar { get; set; }
+        public double ?Salud { get; set; }
+        public double ?Vida { get; set; }
+        public double ?Gadget { get; set; }
+        public double ?Mascota { get; set; }
+        public double ?Viaje { get; set; }
+        public double ?Retiro { get; set; }
+        public double ?Pyme { get; set; }
     }
 }

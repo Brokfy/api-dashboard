c5b5cc16610dc3b2e3a441c9539e5985496875de
godizord <luisleongx@gmail.com>
2020-06-29 13:31:28 -0400

    se actualiza modulo perfil usuario en endpoint ListaUsuario

---
+++ brokfy.dashboard.api/Models/PostDetallePerfilAseguradoModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace brokfy.dashboard.api.Models
+{
+    public class PostDetallePerfilAseguradoModel
+    {
+        public string Username { get; set; }
+        public string Modulo { get; set; }
+        public int IdDetalle { get; set; }
+        public bool Aplica { get; set; }
+    }
+}

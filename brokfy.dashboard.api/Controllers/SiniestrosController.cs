d564b5235d478f72c7abce18669f446a5aa46199
Vianney Gutierrez <vgutierrez@geovictoria.com>
2020-07-05 17:32:07 -0400

    avances siniestros

---
+++ brokfy.dashboard.api/Controllers/SiniestrosController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using brokfy.dashboard.api.data.DataModel;
+using Microsoft.Extensions.Configuration;
+using brokfy.dashboard.api.Models;
+using System;
+
+namespace brokfy.dashboard.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SiniestrosController : ControllerBase
+    {
+        private readonly brokfy_devContext _context;
+        private IConfiguration _config;
+        public SiniestrosController(brokfy_devContext context, IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+        }
+
+        // GET: api/Siniestros
+        [HttpGet]
+        public IEnumerable<Polizas> GetSiniestros()
+        {
+            return _context.Polizas.Where(x => x.IdEstadoPoliza == 3).ToList();
+        }
+
+        // GET: api/PolizasPorConfirmar/4554654
+        [HttpGet("{poliza}")]
+        public Polizas GetSiniestros(string poliza)
+        {
+            return _context.Polizas.Where(x => x.NoPoliza == poliza).FirstOrDefault();
+        }
+    }
+}
